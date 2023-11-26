b6bbb5056e2c3f9e59a7c2de438f751b4b710590
Olena Oliianenko <felangelena@gmail.com>
2023-11-26 16:03:03 +0100

    feat: work server with users, posts, auth

--- index.js
+++ index.js
@@ -64,3 +64,24 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage
 });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .catch((error) => console.log(`${error} did not connect`));
